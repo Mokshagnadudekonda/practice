42ddfe2aae001c73cfe7220d8c315c99241a514c
Mokshagnadudekonda <mokshagnadudekonda@gmail.com>
2025-03-06 15:30:13 +0530

    added new features

---
+++ project/src/components/Movies/Movies.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Movies() {
+    return (
+        <h1>This is Movies Page</h1>
+    )
+}
--- project/src/components/Profile/Profile.js
+++ project/src/components/Profile/Profile.js
@@ -1,18 +1,26 @@
+import { Link } from "react-router-dom"
+
 function Profile() {
     return (
         <nav class="navbar navbar-expand-lg bg-body-primary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">bookmyshow</a>
+                <Link to="/">
+                    <a class="navbar-brand" href="#">bookmyshow</a>
+                </Link>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Movies</a>
+                            <Link to="Movies">
+                                <a class="nav-link active" aria-current="page" href="#">Movies</a>
+                            </Link>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Events</a>
+                            <Link to="Events">
+                                <a class="nav-link" href="#">Events</a>
+                            </Link>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
